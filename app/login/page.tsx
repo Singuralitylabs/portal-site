d6390f2796c81b1b84bca2a4c8619197bfae6fcc
yamada <shinya.yamada@benext-corp.com>
2025-02-23 08:37:19 +0900

    ✨ Googleログインページの実装

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import GoogleLoginButton from "@/components/auth/GoogleLoginButton";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <h1 className="text-3xl font-bold mb-8 text-center">
+        <span className="md:inline block">シンギュラリティ・ラボ </span>
+        <span className="md:inline block">ポータルサイト</span>
+      </h1>
+      <GoogleLoginButton />
+    </div>
+  );
+}
