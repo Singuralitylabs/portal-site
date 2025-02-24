3d254bc10265038692e9cfb0eb4f639a28e614d2
Tomohiro Takahashi <takasann@takasann.com>
2025-02-24 14:22:25 +0900

    資料カードの英語名をdocumentに変更、不要なフォーマットを削除

--- app/documents/components/DocumentCard.tsx
+++ app/documents/components/DocumentCard.tsx
@@ -30,8 +30,7 @@ export function DocumentCard({ document }: DocumentCardProps) {
         <Text component="div" p="1rem" h={(2 * 1.55 + 2) + 'rem'}>{document.description}</Text>
         <Flex gap="0.25rem" justify="flex-start" align="center" direction="row" p="0 1rem 1rem">
           <Calendar style={{width: '1rem', height: '1rem'}} />
-          <Text component="div" fs="0.875rem" lh="1.25rem">{new Date(document.updated_at).toLocaleDateString("ja-JP", {year: "numeric",month: "2-digit",
-   day: "2-digit"}).replaceAll('/', '-')}</Text>
+          <Text component="div" fs="0.875rem" lh="1.25rem">{new Date(document.updated_at).toLocaleDateString("ja-JP", {year: "numeric",month: "2-digit",day: "2-digit"}).replaceAll('/', '-')}</Text>
           <Button component="div" radius="md" size="compact-sm" c="rgb(23,23,23)" bg="gray.2" fs="0.875rem" lh="1.25rem" ml="0.5rem">{document.category}</Button>
         </Flex>
         <Button color="#000" component="a" href={document.url} w={'100% - 2rem'} m="0 1rem 1rem" display="block" target="_blank" rel="noopener noreferrer">
