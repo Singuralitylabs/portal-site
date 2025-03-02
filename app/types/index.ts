b294e5a2bd2f2718581971f5501a58b39984cd72
yamada <shinya.yamada@benext-corp.com>
2025-03-02 10:54:10 +0900

    🏷️ Usersテーブルの型追加

--- app/types/index.ts
+++ app/types/index.ts
@@ -2,3 +2,6 @@ import { Database } from "./lib/database.types";
 
 type DocumentsTable = Database["public"]["Tables"]["documents"];
 export type DocumentType = DocumentsTable["Row"];
+
+type UsersTable = Database["public"]["Tables"]["users"];
+export type UserType = UsersTable["Row"];
