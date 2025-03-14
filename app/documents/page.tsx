1c9ca6b6ff1d6edf4096af21b35148d35a7aa28e
opqrshun <opqrshun@gmail.com>
2025-03-14 09:16:52 +0900

    Documentページ: エラーログが重複していたので削除

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -5,7 +5,6 @@ export default async function DocumentsPage() {
   const { data, error } = await fetchDocuments();
 
   if (error) {
-    console.error('データ取得エラー:', error);
     return <p>データを取得できませんでした。</p>;
   }
 
