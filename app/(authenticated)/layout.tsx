002a0211c8b5ff0b8b3facf7887569f8c55b9f86
Satoshi Inoue <s.inoue.610@gmail.com>
2025-03-14 09:13:53 +0900

    共通レイアウトは残し、authenticatedにサイドバーを追加

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,41 +1,15 @@
-import type { Metadata } from "next";
-import "@mantine/core/styles.css";
-import "./../globals.css";
-import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from "@mantine/core";
+// 認証済みページレイアウト
 import { SideNav } from "./components/SideNav";
 
-export const metadata: Metadata = {
-  title: "Sinlab Portal",
-  description: "シンギュラリティ・ラボのポータルサイトです。",
-  icons: {
-    icon: "/icon.png",
-    shortcut: "/icon.png",
-    apple: "/apple-icon.png",
-    other: {
-      rel: "apple-touch-icon",
-      url: "/apple-icon.png",
-    },
-  },
-};
-
-export default function RootLayout({
+export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" {...mantineHtmlProps}>
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <MantineProvider>
-          <div className="sm:flex min-h-screen">
-            <SideNav />
-            <div className="flex-1 sm:ml-48">{children}</div>
-          </div>
-        </MantineProvider>
-      </body>
-    </html>
+    <div className="sm:flex min-h-screen">
+      <SideNav />
+      <div className="flex-1 sm:ml-48">{children}</div>
+    </div>
   );
 }
