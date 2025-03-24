56c6d49f604728fb3cab999f5c79e21cd1777a92
yamada <shinya.yamada@benext-corp.com>
2025-03-24 11:30:11 +0900

    ✨ supabaseのセッション変数にclerk_idを保存する処理を追加

--- app/services/api/supabase.ts
+++ app/services/api/supabase.ts
@@ -1,9 +1,25 @@
-import { createClient } from '@supabase/supabase-js';
+import { currentUser } from "@clerk/nextjs/server";
+import { createClient } from "@supabase/supabase-js";
 
-// Supabaseクライアントの作成
+export async function createServerSupabaseClient() {
+  const user = await currentUser();
+  const userId = user?.id || "";
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+  );
+
+  if (userId) {
+    await supabase.rpc("set_clerk_user_id", { clerk_id: userId });
+  }
+
+  return supabase;
+}
+
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
 );
 
 export default supabase;
