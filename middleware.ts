34619df6d8a0ffa88f3c2082a2d84e205b4fbd8a
Satoshi Inoue <s.inoue.610@gmail.com>
2025-05-31 16:00:59 +0900

    [draft] middle処理追加

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,51 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { currentUser, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { createClient } from "@supabase/supabase-js";
 
 const isPublicRoute = createRouteMatcher([
   "/login(.*)",
   "/sso-callback",
   "/services/api/webhooks(.*)",
+  "/unapproved",
+  "/rejected",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
+    // Protect all routes except public ones
     await auth.protect();
+
+    const current_user = await currentUser();
+    const userId = current_user?.id || "";
+
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+    );
+
+    // Clerk上にユーザーIDが存在しない場合はログインページへリダイレクト
+    if (!userId) {
+      return Response.redirect(new URL("/login", request.url));
+    }
+
+    // SupabaseのUsersテーブルからユーザー情報を取得
+    const { data: user, error } = await supabase
+      .from("Users")
+      .select("status")
+      .eq("clerk_user_id", userId) // ClerkのユーザーIDで検索
+      .single();
+
+    if (error || !user) {
+      // Usersテーブルにユーザーが見つからない場合はログインページへ
+      return Response.redirect(new URL("/login", request.url));
+    }
+
+    if (user.status !== "pending") {
+      return Response.redirect(new URL("/unapproved", request.url));
+    }
+    if (user.status === "reject") {
+      return Response.redirect(new URL("/rejected", request.url));
+    }
+    // statusがactiveならそのまま
   }
 });
 
