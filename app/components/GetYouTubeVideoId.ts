51a9898d6d6cce578406247f1fee91f300d0345a
takasann0 <takasann@takasann.com>
2025-06-26 20:47:17 +0900

    動画一覧ページのサムネイル画像URL取得を関数化した。サムネイル画像のアスペクト比設定などのCSSを簡略化した

--- app/components/GetYouTubeVideoId.ts
+++ app/components/GetYouTubeVideoId.ts
@@ -1,10 +1,10 @@
 'use client';
 
-interface YoutubeVideoIdProps {
+interface YouTubeVideoIdProps {
   url: string;
 }
 
-export function YoutubeVideoId({ url }: YoutubeVideoIdProps): string | undefined  {
+export function GetYouTubeVideoId({ url }: YouTubeVideoIdProps): string | undefined  {
   const match = url.match(/(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([\w-]{11})/);
   return match?.[1];
 }
