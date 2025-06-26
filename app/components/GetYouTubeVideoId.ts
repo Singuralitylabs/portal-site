69eb0843a5793bb0795cfbc648fbc011544353ed
takasann0 <takasann@takasann.com>
2025-06-26 21:07:49 +0900

    YouTubeのサムネイル画像取得対応、VideoId取得の関数化、サムネイル画像URL取得の関数化、サムネイル画像のCSS整理を行いました。

---
+++ app/components/GetYouTubeVideoId.ts
@@ -0,0 +1,10 @@
+'use client';
+
+interface YouTubeVideoIdProps {
+  url: string;
+}
+
+export function GetYouTubeVideoId({ url }: YouTubeVideoIdProps): string | undefined  {
+  const match = url.match(/(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([\w-]{11})/);
+  return match?.[1];
+}
