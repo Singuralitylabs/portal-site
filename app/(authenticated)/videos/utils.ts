9992ef3019c5e094e3d4bd4034ac83bfa1bc9f94
takasann0 <takasann@takasann.com>
2025-06-26 21:52:28 +0900

    YoutubeのVideoIdの関数配置を変更、Tailwind CSSの動画サムネイル表示について整理

--- app/(authenticated)/videos/utils.ts
+++ app/(authenticated)/videos/utils.ts
@@ -4,7 +4,7 @@ interface YouTubeVideoIdProps {
   url: string;
 }
 
-export function GetYouTubeVideoId({ url }: YouTubeVideoIdProps): string | undefined  {
+export function getYouTubeVideoId({ url }: YouTubeVideoIdProps): string | undefined  {
   const match = url.match(/(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([\w-]{11})/);
   return match?.[1];
 }
