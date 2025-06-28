1852c7f0d1f6eee0e1ff13462705fcccdd2aa3c8
Satoshi Inoue <s.inoue.610@gmail.com>
2025-06-28 15:10:21 +0900

    カテゴリー名をcategory_idから取得

--- app/(authenticated)/documents/components/DocumentCard.tsx
+++ app/(authenticated)/documents/components/DocumentCard.tsx
@@ -31,7 +31,9 @@ export function DocumentCard({ document }: DocumentCardProps) {
         <Flex gap="0.25rem" justify="flex-start" align="center" direction="row" p="0 1rem 1rem">
           <Calendar style={{width: '1rem', height: '1rem'}} />
           <Text component="div" fs="0.875rem" lh="1.25rem">{new Date(document.updated_at).toLocaleDateString("ja-JP", {year: "numeric",month: "2-digit",day: "2-digit"}).replaceAll('/', '-')}</Text>
-          <Button component="div" radius="md" size="compact-sm" c="rgb(23,23,23)" bg="gray.2" fs="0.875rem" lh="1.25rem" ml="0.5rem">{document.category}</Button>
+          <Button component="div" radius="md" size="compact-sm" c="rgb(23,23,23)" bg="gray.2" fs="0.875rem" lh="1.25rem" ml="0.5rem">
+            {document.category?.name}
+          </Button>
         </Flex>
         <Button color="#000" component="a" href={document.url} w={'100% - 2rem'} m="0 1rem 1rem" display="block" target="_blank" rel="noopener noreferrer">
           資料を開く
