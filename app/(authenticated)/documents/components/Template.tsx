14c5a0a7289948a22e30151049a4400d9ffe5864
miyazawa-sinlab <t-miyazawa@future-tech-association.org>
2025-07-11 19:39:38 +0900

    資料一覧のカテゴリー別表示

--- app/(authenticated)/documents/components/Template.tsx
+++ app/(authenticated)/documents/components/Template.tsx
@@ -5,24 +5,36 @@ import { Grid, Paper } from '@mantine/core';
 import { PageTitle } from '@/app/components/PageTitle';
 
 import { DocumentWithCategoryType } from '@/app/types';
+import { CategoryType } from '@/app/types';
 
 interface DocumentsPageTemplateProps  {
     documents: DocumentWithCategoryType[];
+    categories: CategoryType[];
 };
 
-export function DocumentsPageTemplate({documents} : DocumentsPageTemplateProps) {
+export function DocumentsPageTemplate({ documents, categories } : DocumentsPageTemplateProps) {
+  const documentCategoryNames = new Set(documents.map((document) => document.category?.name));
+  const existingCategories = categories.filter((category) => documentCategoryNames.has(category.name));
+
   return (
     <Paper m="0 2rem">
       <PageTitle>資料一覧</PageTitle>
 
       <Paper>
-        <Grid>
-          {documents.map((document) => (
-            <Grid.Col span={{ base: 12, md: 6, lg: 4 }} key={document.id + '_grid'}>
-              <DocumentCard document={document} />
-            </Grid.Col>
-          ))}
-        </Grid>
+        {existingCategories.map((category) => (
+          <div key={category.id}>
+            <h2>{category.name}</h2>
+            <Grid>
+              {documents.filter((document) => document.category?.name === category.name)
+                .map((document) => (
+                  <Grid.Col span={{ base: 12, md: 6, lg: 4 }} key={document.id + '_grid'}>
+                    <DocumentCard document={document} />
+                  </Grid.Col>
+              ))}
+            </Grid>
+            <br/>
+          </div>
+        ))}
       </Paper>
     </Paper>
   );
--- app/(authenticated)/documents/page.tsx
+++ app/(authenticated)/documents/page.tsx
@@ -1,12 +1,14 @@
 import { fetchDocuments } from '@/app/services/api/documents';
+import { fetchCategoriesByType } from '@/app/services/api/categories';
 import { DocumentsPageTemplate } from './components/Template';
 
 export default async function DocumentsPage() {
   const { data, error } = await fetchDocuments();
+  const { data:dataCategory, error:errorCategory } = await fetchCategoriesByType("documents");
 
-  if (error) {
+  if (error || errorCategory) {
     return <p>データを取得できませんでした。</p>;
   }
 
-  return <DocumentsPageTemplate documents={data} />;
+  return <DocumentsPageTemplate documents={data} categories={dataCategory} />;
 }
