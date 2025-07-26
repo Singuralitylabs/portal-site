5f418bdaaf597ef967a48be94a0f246f50e99b9b
yamada <shinya.yamada@benext-corp.com>
2025-07-26 12:54:57 +0900

    🗃️ Supabase Authへの移行に伴うpolicyの修正

--- supabase/migrations/04_policies/videos_policies.sql
+++ supabase/migrations/04_policies/videos_policies.sql
@@ -1,12 +1,12 @@
 -- videosテーブルのポリシー
 
--- 登録済みユーザーは全てのvideosを閲覧可能
-DROP POLICY IF EXISTS "registered_users_can_read_videos" ON "videos";
-CREATE POLICY "registered_users_can_read_videos" ON "videos"
+-- 認証済みユーザーは全てのvideosを閲覧可能
+DROP POLICY IF EXISTS "authenticated_users_can_read_videos" ON "videos";
+CREATE POLICY "authenticated_users_can_read_videos" ON "videos"
   FOR SELECT
+  TO authenticated
   USING (
-    is_registered_user()
-    AND is_deleted = FALSE
+    is_deleted = FALSE
   );
 
 ALTER TABLE "videos" ENABLE ROW LEVEL SECURITY;
