62e15ed10465b7765ad5de5de2450729d2a544f7
yamada <shinya.yamada@benext-corp.com>
2025-08-30 12:55:39 +0900

    ✨ メンバーリストページの追加

--- app/constants/user.ts
+++ app/constants/user.ts
@@ -1,7 +1,13 @@
-import { UserStatusType } from "../types";
+import { UserRoleType, UserStatusType } from "../types";
 
 export const USER_STATUS: Record<string, UserStatusType> = {
   PENDING: "pending",
   ACTIVE: "active",
   REJECTED: "rejected",
+} as const;
+
+export const USER_ROLE: Record<string, UserRoleType> = {
+  ADMIN: "admin",
+  MAINTAINER: "maintainer",
+  MEMBER: "member",
 } as const;
