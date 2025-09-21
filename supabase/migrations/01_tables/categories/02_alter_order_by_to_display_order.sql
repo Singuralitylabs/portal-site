56de91a623eb0a964d865034d154a595a822d170
miyazawa-sinlab <t-miyazawa@future-tech-association.org>
2025-09-21 10:19:33 +0900

    レビュー指摘対応

--- supabase/migrations/01_tables/categories/02_alter_order_by_to_display_order.sql
+++
@@ -1,3 +0,0 @@
---  order_byカラムをdisplay_orderに変更
-ALTER TABLE categories
-RENAME COLUMN order_by TO display_order;
--- supabase/migrations/01_tables/videos/04_alter_order_by_to_display_order.sql
+++
@@ -1,3 +0,0 @@
---  order_byカラムをdisplay_orderに変更
-ALTER TABLE videos
-RENAME COLUMN order_by TO display_order;
