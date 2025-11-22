33ef633d695fade5cd610ce9eaaff5287c01a6b0
yamada <shinya.yamada@benext-corp.com>
2025-11-23 08:52:40 +0900

    ✨アプリ削除機能の追加

--- app/(authenticated)/applications/page.tsx
+++ app/(authenticated)/applications/page.tsx
@@ -1,12 +1,27 @@
 import { fetchApplications } from "@/app/services/api/applications-server";
 import { fetchCategoriesByType } from "@/app/services/api/categories-server";
 import { ApplicationsPageTemplate } from "./components/Template";
+import { checkContentPermissions } from "@/app/services/auth/permissions";
+import { fetchUserInfoByAuthId } from "@/app/services/api/users-server";
+import { getServerCurrentUser } from "@/app/services/api/supabase-server";
 
 export default async function ApplicationsPage() {
+  // サーバーサイドで利用ユーザー情報を参照
+  const { authId, error: currentUserError } = await getServerCurrentUser();
+  if (currentUserError) {
+    console.error("認証情報の取得に失敗:", currentUserError);
+    return (
+      <div className="text-center py-8">
+        <p className="text-red-600 text-xl">認証情報を取得できませんでした。</p>
+      </div>
+    );
+  }
+
   // アプリデータ・カテゴリーデータを並列取得
-  const [applicationsResult, categoriesResult] = await Promise.all([
+  const [applicationsResult, categoriesResult, userResult] = await Promise.all([
     fetchApplications(),
     fetchCategoriesByType("applications"),
+    fetchUserInfoByAuthId({ authId: authId }),
   ]);
 
   const { data: applications, error: applicationError } = applicationsResult;
@@ -29,5 +44,22 @@ export default async function ApplicationsPage() {
     );
   }
 
-  return <ApplicationsPageTemplate applications={applications} categories={categories} />;
+  const { id: userId, role, error: roleError } = userResult;
+  if (roleError || !role || !userId) {
+    console.error("ユーザー情報の取得に失敗:", roleError);
+    return (
+      <div className="text-center py-8">
+        <p className="text-red-600 text-xl">ユーザー情報を取得できませんでした。</p>
+      </div>
+    );
+  }
+
+  return (
+    <ApplicationsPageTemplate
+      applications={applications}
+      categories={categories}
+      isContentMgr={checkContentPermissions(role)}
+      userId={userId}
+    />
+  );
 }
