995ee1e0308c854ad7993da3288998c899e073d8
yamada <shinya.yamada@benext-corp.com>
2025-12-12 08:03:39 +0900

    🎨 ApprovalTemplate.tsxの削除

--- app/(authenticated)/dashboard/components/Template.tsx
+++ app/(authenticated)/dashboard/components/Template.tsx
@@ -1,11 +1,31 @@
-import ApprovalTemplate from "@/app/(authenticated)/dashboard/components/ApprovalTemplate";
+import { PageTitle } from "@/app/components/PageTitle";
 import { MemberAdminType } from "@/app/types";
+import { MemberApprovalCard } from "./MemberCardApproval";
 
-export default function DashboardPageTemplate({ members, adminId }: { members: MemberAdminType[], adminId: number }) {
-    return (
-        <div className="p-6">
-            <h1 className="text-2xl font-bold mb-6">ダッシュボード</h1>
-            <ApprovalTemplate members={members} adminId={adminId} />
+export default function DashboardPageTemplate({
+  members,
+  adminId,
+}: {
+  members: MemberAdminType[];
+  adminId: number;
+}) {
+  return (
+    <div className="p-6">
+      <PageTitle>ダッシュボード</PageTitle>
+      <div className="p-4 overflow-x-hidden">
+        <h2 className="scroll-mt-40">承認管理</h2>
+        <div>
+          {members.length > 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 lg:gap-8 mb-8">
+              {members.map(member => (
+                <MemberApprovalCard key={member.id} member={member} adminId={adminId} />
+              ))}
+            </div>
+          ) : (
+            <p className="text-gray-500 mt-4">承認待ちのユーザーはいません。</p>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
