d0d4c39a0ef1263b50bcb874efbe9af9746e7ba6
yamada <shinya.yamada@benext-corp.com>
2025-12-13 17:13:03 +0900

    ⚡️ 今より前後3ヶ月間を先に取得する仕様に変更

--- app/(authenticated)/calendar/components/Template.tsx
+++ app/(authenticated)/calendar/components/Template.tsx
@@ -4,14 +4,24 @@ import { CalendarView } from "./CalendarView";
 
 interface CalendarPageTemplateProps {
   events: CalendarEvent[];
+  fetchedStartDate: Date;
+  fetchedEndDate: Date;
 }
 
-export function CalendarPageTemplate({ events }: CalendarPageTemplateProps) {
+export function CalendarPageTemplate({
+  events,
+  fetchedStartDate,
+  fetchedEndDate,
+}: CalendarPageTemplateProps) {
   return (
     <>
       <PageTitle>シンラボカレンダー</PageTitle>
       <div className="m-2">
-        <CalendarView events={events} />
+        <CalendarView
+          events={events}
+          fetchedStartDate={fetchedStartDate}
+          fetchedEndDate={fetchedEndDate}
+        />
       </div>
     </>
   );
